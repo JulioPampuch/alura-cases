985814edf805d7292d8b8a3c3c6d329a912d9eae
Julio Pampuch <juliopampuch@gmail.com>
2023-01-09 21:41:02 -0300

    testing[

--- pages/_app.js
+++ pages/_app.js
@@ -8,23 +8,18 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         {/* <!-- Google tag (gtag.js) --> */}
-        {/* <Script async src="https://www.googletagmanager.com/gtag/js?id=G-H5X5BT3K8E"></Script>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-H5X5BT3K8E"></Script>
         <Script
           dangerouslySetInnerHTML={{
             __html: `
-
-          `
-          }}
-        /> */}
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-H5X5BT3K8E">
-          {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
         
           gtag('config', 'G-H5X5BT3K8E');
-        `}
-        </Script>
+          `
+          }}
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600;700&display=swap" rel="stylesheet" />
