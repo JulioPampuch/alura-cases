45654e254c7ab0eb838b499510fcf4b938fe42e6
Julio Pampuch <juliopampuch@gmail.com>
2023-01-09 21:33:59 -0300

    testing

--- pages/_app.js
+++ pages/_app.js
@@ -21,7 +21,7 @@ export default function App({ Component, pageProps }) {
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-
+        
           gtag('config', 'G-H5X5BT3K8E');
         `}
         </Script>
